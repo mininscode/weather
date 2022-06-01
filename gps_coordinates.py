584669d123444f1ad7a35b356341b39619e6effc
mininscode <mininscode@gmail.com>
2022-06-01 22:10:45 +0300

    MVP: app prints weather in console

--- gps_coordinates.py
+++ gps_coordinates.py
@@ -1,5 +1,10 @@
+import geocoder
 from dataclasses import dataclass
 
+import config
+from exceptions import CantGetCoordinates
+
+IpOwner = 'me'
 
 @dataclass(slots=True, frozen=True)
 class Coordinates:
@@ -8,4 +13,26 @@ class Coordinates:
 
 def get_coordinates() -> Coordinates:
     """Returns current coordinates using MacBook GPS"""
-    return Coordinates(longitude=10, latitude=20)
+    coordinates = _get_geocoder_gps_coordinates()
+    return _round_coordinates(coordinates)
+
+def _get_geocoder_gps_coordinates() -> Coordinates:
+    try:
+        geocode = geocoder.ip(IpOwner)
+    except Exception:
+        raise CantGetCoordinates
+    latitude, longitude = geocode.latlng
+    if latitude is None or longitude is None:
+        raise CantGetCoordinates
+    return Coordinates(latitude=latitude, longitude=longitude)
+   
+def _round_coordinates(coordinates: Coordinates) -> Coordinates:
+    if not config.USE_ROUNDED_COORDS:
+        return coordinates
+    return Coordinates(*map(
+            lambda coord: round(coord, 2),
+            [coordinates.latitude, coordinates.longitude]
+        ))
+
+if __name__ == "__main__":
+    print(get_coordinates())
